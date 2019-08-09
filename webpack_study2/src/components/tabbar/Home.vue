7a57a06573f30dfaa9f1f6a081ad0b03db990064
naruto2205200 <perfect_zhangxf@163.com>
2019-08-09 18:01:09 +0800

    11

--- webpack_study2/src/components/tabbar/Home.vue
+++ webpack_study2/src/components/tabbar/Home.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        <h1>这是home</h1>
+        <!-- 轮播图 -->
         <mt-swipe :auto="4000">
             <mt-swipe-item v-for="item in lunbolist" :key="item.id">
-                <img :src="item.headPortrait" alt="" />
+                <!-- <img :src="item.headPortrait" alt="" /> -->
                 <!-- <img src="../../images/0d4ad6027c3e143399bd2188fd61504b.jpg" alt=""> -->
+                <span>{{ item.name }}</span>
             </mt-swipe-item>
             <!-- <mt-swipe-item>1</mt-swipe-item>
             <mt-swipe-item>2</mt-swipe-item>
             <mt-swipe-item>3</mt-swipe-item> -->
         </mt-swipe>
+         <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-home"></span>
+		                    <div class="mui-media-body">Home</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+		                    <div class="mui-media-body">Email</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-chatbubble"></span>
+		                    <div class="mui-media-body">Chat</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-location"></span>
+		                    <div class="mui-media-body">location</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-search"></span>
+		                    <div class="mui-media-body">Search</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-phone"></span>
+		                    <div class="mui-media-body">Phone</div></a></li>
+		        </ul> 
     </div>
 </template>
 <script>
@@ -54,4 +75,8 @@ export default {
     .mint-swipe-item:nth-child(3) {
         background-color: blue;
     } */
+
+    .mui-grid-view .mui-grid-9 {
+        background-color: red;
+    }
 </style>
