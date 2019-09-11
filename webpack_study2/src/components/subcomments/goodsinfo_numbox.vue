4b055f0c6d31ef4fab3b38e49ae1d9b58eb819f5
naruto2205200 <perfect_zhangxf@163.com>
2019-09-11 18:08:08 +0800

    11

---
+++ webpack_study2/src/components/subcomments/goodsinfo_numbox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+            <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+            <input id="test" class="mui-input-numbox" type="number" value="1" />
+            <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+        </div>
+    </div>
+</template>
+<script>
+import mui from '../../lib/mui/js/mui.min.js'
+export default {
+    mounted() {
+        // 初始化数字选择框
+        mui('.mui-numbox').numbox()
+    },
+}
+</script>
+<style scoped>
+    
+</style>
