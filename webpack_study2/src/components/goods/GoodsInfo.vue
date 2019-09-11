14778e3a9c3eb5b3c0357ffbb1b0129904d26ae4
naruto2205200 <perfect_zhangxf@163.com>
2019-09-11 18:07:47 +0800

    购物区域

--- webpack_study2/src/components/goods/GoodsInfo.vue
+++ webpack_study2/src/components/goods/GoodsInfo.vue
@@ -1,25 +1,36 @@
 <template>
     <div>
         <div class="mui-card">
-				<div class="mui-card-content">
-					<div class="mui-card-content-inner">
-						<swiper :lunbolist="lunbolist" :isfull="false"></swiper>
-					</div>
-				</div>
-			</div>
-			<div class="mui-card">
-				<div class="mui-card-header">页眉</div>
-				<div class="mui-card-content">
-					<div class="mui-card-content-inner">
-						包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）
-					</div>
-				</div>
-				<div class="mui-card-footer">页脚</div>
-			</div>
+            <div class="mui-card-content">
+                <div class="mui-card-content-inner">
+                    <swiper :lunbolist="lunbolist" :isfull="false"></swiper>
+                </div>
+            </div>
+        </div>
+        <!-- 商品区域 -->
+        <div class="mui-card">
+            <div class="mui-card-header">页眉</div>
+            <div class="mui-card-content">
+                <div class="mui-card-content-inner">
+                    <p class="price">
+                        市场价：<del>￥2399</del>销售价：<span class="now_price">￥2199</span> 
+                    </p>
+                    <p>
+                        购买数量：<numbox></numbox>
+                    </p>
+                    <p>
+                        <mt-button type="primary" size="small">立即购买</mt-button>
+                        <mt-button type="danger" size="small">加入购物车</mt-button>
+                    </p>
+                </div>
+            </div>
+            <div class="mui-card-footer">页脚</div>
+        </div>
     </div>
 </template>
 <script>
 import swiper from '../subcomments/swiper.vue'
+import numbox from '../subcomments/goodsinfo_numbox.vue'
 export default {
     data() {
         return {
@@ -39,8 +50,20 @@ export default {
             })
         }
     },
+    components: {
+        swiper,
+        numbox
+    }
 }
 </script>
 <style scoped>
-
+    .goodsinfo-conainer {
+        background-color: #eee;
+        overflow: hidden;
+    }
+    .now_price {
+        color: red;
+        font-size: 16px;
+        font-weight: bold;
+    }
 </style>
